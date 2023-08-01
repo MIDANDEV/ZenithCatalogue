4c7013511e60524a437eb80b658f2df7cd49c93a
MIDAN <mhsawadogo99@gmail.com>
2023-08-01 16:06:33 +0200

    fix admin zenithcatalogue

--- catalogueApp/admin.py
+++ catalogueApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','marque','image','categorie','price','detail','created_at')
+
+
+admin.site.register(Product, ProductAdmin  )
