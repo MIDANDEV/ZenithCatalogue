c2eca574b482f399c51fb69b4689458d08aecb93
MIDAN <mhsawadogo99@gmail.com>
2023-08-02 14:15:10 +0200

    fix responsive design card & app url

--- ZenithCatalogue/urls.py
+++ ZenithCatalogue/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('catalogueApp.urls'))
+    path('', include('catalogueApp.urls'))
 ]
 if settings.DEBUG:
 
