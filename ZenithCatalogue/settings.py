6b0dc5fb98c09c308c2fc4482bceee3c05400c59
MIDAN <mhsawadogo99@gmail.com>
2023-11-24 12:57:24 +0100

    config vercel deployement

--- ZenithCatalogue/settings.py
+++ ZenithCatalogue/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--hmw$a_cuo6@q+#!m=xl59hfhd##a(1)8zhg##3(zwz7ct_2pf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1']
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
@@ -81,10 +81,21 @@ WSGI_APPLICATION = 'ZenithCatalogue.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER':'postgres',
+        'PASSWORD':'f64aCdg32BggFA*B56*c44E2DeE5cdgB',
+        'HOST':'monorail.proxy.rlwy.net',
+        'PORT':'21901'
     }
 }
 
@@ -126,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
-
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
